a1719728a08fb8734f4de1d746a41915258bf808
Joel Ala-aho <joel.ala-aho@netti.fi>
2018-12-03 13:04:04 +0200

    ProjectMath.html

--- ProjectMath.js
+++ ProjectMath.js
@@ -1,5 +1,42 @@
 //Number system conversions
 
+function numberConverter(source,valNum) 
+{
+    valNum = parseFloat(valNum);
+    var inputBinary = document.getElementById("inputBinary");
+    var inputDecimal = document.getElementById("inputDecimal");
+    var inputOctal = document.getElementById("inputOctal");
+    var inputHexadecimal = document.getElementById("inputHexadecimal");
+
+    if (source=="inputBinary") 
+    {
+      inputDecimal.value=(parseInt(valNum, 2));
+      inputOctal.value=(parseInt(valNum, 2).toString(8));
+      inputHexadecimal.value=(parseInt(valNum, 2).toString(16));
+    }
+
+    if (source=="inputDecimal") 
+    {
+        inputBinary.value=(parseInt(valNum, 10).toString(2));
+        inputOctal.value=(parseInt(valNum, 10).toString(8));
+        inputHexadecimal.value=(parseInt(valNum, 10).toString(16));
+    }
+
+    if (source=="inputOctal") 
+    {        
+        inputBinary.value=(parseInt(valNum, 8).toString(2));
+        inputDecimal.value=(parseInt(valNum, 8).toString(10));
+        inputHexadecimal.value=(parseInt(valNum, 8).toString(16));
+    }
+
+    if (source=="inputHexadecimal") 
+    {        
+        inputBinary.value=(parseInt(valNum, 16).toString(2));
+        inputDecimal.value=(parseInt(valNum, 16).toString(10));
+        inputOctal.value=(parseInt(valNum, 16).toString(8));
+    }
+}
+
 //Number system outputs
 
 //Combinatorics

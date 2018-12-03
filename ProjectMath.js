9f25b245936e64e845751219918d6bbc4b7508d6
Joel Ala-aho <joel.ala-aho@netti.fi>
2018-12-03 19:58:51 +0200

    ProjectMath.css

--- ProjectMath.js
+++ ProjectMath.js
@@ -2,7 +2,6 @@
 
 function numberConverter(source,valNum) 
 {
-    valNum = parseFloat(valNum);
     var inputBinary = document.getElementById("inputBinary");
     var inputDecimal = document.getElementById("inputDecimal");
     var inputOctal = document.getElementById("inputOctal");
@@ -10,32 +9,112 @@ function numberConverter(source,valNum)
 
     if (source=="inputBinary") 
     {
-      inputDecimal.value=(parseInt(valNum, 2));
-      inputOctal.value=(parseInt(valNum, 2).toString(8));
-      inputHexadecimal.value=(parseInt(valNum, 2).toString(16));
+        if (inputBinary.value > 0)
+        {
+            var testingBin = (/^[01-]+$/.test(valNum))
+        
+            if (testingBin == 1)
+            {
+            inputDecimal.value=(parseInt(valNum, 2));
+            inputOctal.value=(parseInt(valNum, 2).toString(8));
+            inputHexadecimal.value=(parseInt(valNum, 2).toString(16).toUpperCase());
+            }
+
+            else
+            {
+                alert("ONLY BINARY NUMBERS PLEASE")
+                document.getElementById("inputBinary").value="";
+            }
+        }
+
+        else
+        {
+            return
+        }
     }
 
     if (source=="inputDecimal") 
     {
-        inputBinary.value=(parseInt(valNum, 10).toString(2));
-        inputOctal.value=(parseInt(valNum, 10).toString(8));
-        inputHexadecimal.value=(parseInt(valNum, 10).toString(16));
+        if (inputDecimal.value > 0)
+        {
+
+            var testingDecimal = (/^[0123456789-]+$/.test(valNum))
+
+            if (testingDecimal == 1)
+            {
+                inputBinary.value=(parseInt(valNum, 10).toString(2));
+                inputOctal.value=(parseInt(valNum, 10).toString(8));
+                inputHexadecimal.value=(parseInt(valNum, 10).toString(16).toUpperCase());
+            }
+
+            else
+            {
+                alert("ONLY DECIMAL NUMBERS PLEASE")
+                document.getElementById("inputDecimal").value="";
+            }
+        }
+
+        else
+        {
+            return
+        }
     }
 
     if (source=="inputOctal") 
-    {        
-        inputBinary.value=(parseInt(valNum, 8).toString(2));
-        inputDecimal.value=(parseInt(valNum, 8).toString(10));
-        inputHexadecimal.value=(parseInt(valNum, 8).toString(16));
+    {   
+        if (inputOctal.value > 0)
+        {
+            var testingOctal = (/^[01234567-]+$/.test(valNum))
+
+            if (testingOctal == 1)
+            {
+                inputBinary.value=(parseInt(valNum, 8).toString(2));
+                inputDecimal.value=(parseInt(valNum, 8).toString(10));
+                inputHexadecimal.value=(parseInt(valNum, 8).toString(16).toUpperCase());
+            }
+
+            else
+            {
+                alert("ONLY OCTAL NUMBERS PLEASE")
+                document.getElementById("inputOctal").value="";
+            }
+        }
+
+        else
+        {
+            return
+        }
     }
 
     if (source=="inputHexadecimal") 
-    {        
-        inputBinary.value=(parseInt(valNum, 16).toString(2));
-        inputDecimal.value=(parseInt(valNum, 16).toString(10));
-        inputOctal.value=(parseInt(valNum, 16).toString(8));
+    {   
+        var HexTest = inputHexadecimal.toString();
+
+        if (HexTest.length > 0)
+        {
+            var testingHexadecimal = (/^[0123456789ABCDEFabcdef-]+$/.test(valNum))
+
+            if (testingHexadecimal == 1)
+            {
+                inputBinary.value=(parseInt(valNum, 16).toString(2));
+                inputDecimal.value=(parseInt(valNum, 16));
+                inputOctal.value=(parseInt(valNum, 16).toString(8));
+            }
+
+            else
+            {
+                alert("ONLY HEXADECIMAL NUMBERS PLEASE")
+                document.getElementById("inputHexadecimal").value="";
+            }
+        }   
+
+        else
+        {
+            return
+        }
     }
 }
+
 
 //Number system outputs
 

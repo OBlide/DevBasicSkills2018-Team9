9e3882aa2d6cb84026add86f8bbe13ab9176fb2c
Joel Ala-aho <joel.ala-aho@netti.fi>
2018-12-07 14:20:27 +0200

    Did some work on the permutations without replacement

--- permutationFunction.js
+++ permutationFunction.js
@@ -3,25 +3,28 @@ function permutationFunction()
     var n = document.getElementById("inputElements").value;
     var r = document.getElementById("inputSubset").value;
     var checkBox = document.getElementById("selectReplacement");
-    var answer2 = 0;
 
     if (checkBox.checked == true)
     {
-        for (i = 0; i < n; i++) 
+        for (i = n - 1; i > 0; i--) 
         {
-            
+            n = n * i;
         }
+
+        var result2 = n;
+        document.getElementById("result2").innerHTML = "Permutations with replacement:" + result2;
         
     }
 
     else    
     {
-        for (i = 0; i < n; i++) 
+        for (i = n - 1; i > 0; i--) 
         {
-            answer2 = n * (n - 1)
+            n = n * i;
         }
 
- 
+        var result2 = n;
+        document.getElementById("result2").innerHTML = "Permutations:" + result2;
     }
 
 }
